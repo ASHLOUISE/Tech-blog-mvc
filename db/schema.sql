a8d12a904e9aadeb4520ae43a5538739ed69d3f5
ASHLOUISE <paluzziashley@gmail.com>
2024-03-17 20:46:30 -0700

    schema added

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS tech_blog_mvc;
+CREATE DATABASE tech_blog_mvc;
--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const app = express();
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
 
 
 const hbs = exphbs.create({ helpers });
